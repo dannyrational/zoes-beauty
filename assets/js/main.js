df9da5da3b5e0465d9b7c89fe47ff49fa8d5550b
Daniel Rodas <drodas6232@gmail.com>
2021-09-04 23:28:56 -0600

    9: Completed Contact Form Mobile Ver

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ const mobileMenuBtnOpen = document.querySelector("#mobileMenuBtnOpen");
 const mobileMenuBtnClose = document.querySelector("#mobileMenuBtnClose");
 const logoWhite = document.querySelector("#logoWhite");
 const logoBlack = document.querySelector("#logoBlack");
+const navbarBorder = document.querySelector("#navbarBorder");
+
 
 const slideOutMenu = document.querySelector("#slideOutMenu");
 const bodyWrap = document.querySelector("#bodyWrap");
@@ -11,41 +13,41 @@ const bodyWrap = document.querySelector("#bodyWrap");
 // ? ***** Navbar Color Change on Scroll *****
 window.addEventListener('scroll', function () {
     if (document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
-        mobileNavbar.classList.add('border-b')
+        navbarBorder.classList.add('border-b');
         mobileNavbar.classList.add('bg-white');
         mobileMenuBtnOpen.classList.remove('text-white');
         mobileMenuBtnClose.classList.remove('text-white');
         logoWhite.classList.add('hidden');
         logoBlack.classList.remove('hidden');
     } else {
-        mobileNavbar.classList.remove('border-b')
+        navbarBorder.classList.remove('border-b')
         mobileNavbar.classList.remove('bg-white');
         mobileMenuBtnOpen.classList.add('text-white');
         mobileMenuBtnClose.classList.add('text-white');
-        logoWhite.classList.remove('hidden')
+        logoWhite.classList.remove('hidden');
         logoBlack.classList.add('hidden');
     };
 });
 
 // ? ***** Slide Out Menu *****
 mobileMenuBtnOpen.addEventListener("click", function () {
-    slideOutMenu.classList.remove('-right-full')
-    slideOutMenu.classList.add('right-0')
-    bodyWrap.classList.remove('left-0')
-    bodyWrap.classList.add('-left-3/4')
-    mobileNavbar.classList.remove('left-0')
-    mobileNavbar.classList.add('-left-3/4')
+    slideOutMenu.classList.remove('-right-full');
+    slideOutMenu.classList.add('right-0');
+    bodyWrap.classList.remove('left-0');
+    bodyWrap.classList.add('-left-3/4');
+    mobileNavbar.classList.remove('left-0');
+    mobileNavbar.classList.add('-left-3/4');
     mobileMenuBtnOpen.classList.add('hidden');
     mobileMenuBtnClose.classList.remove('hidden');
 });
 
 mobileMenuBtnClose.addEventListener("click", function () {
-    slideOutMenu.classList.remove('right-0')
-    slideOutMenu.classList.add('-right-full')
-    bodyWrap.classList.remove('-left-3/4')
-    bodyWrap.classList.add('left-0')
-    mobileNavbar.classList.remove('-left-3/4')
-    mobileNavbar.classList.add('left-0')
+    slideOutMenu.classList.remove('right-0');
+    slideOutMenu.classList.add('-right-full');
+    bodyWrap.classList.remove('-left-3/4');
+    bodyWrap.classList.add('left-0');
+    mobileNavbar.classList.remove('-left-3/4');
+    mobileNavbar.classList.add('left-0');
     mobileMenuBtnOpen.classList.remove('hidden');
     mobileMenuBtnClose.classList.add('hidden');
 });
